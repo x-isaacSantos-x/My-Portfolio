abf8d08faa874acf44a5914c64a83c91b02253ee
Isaac Mendes <isaac.misionero18@gmail.com>
2025-11-03 14:49:06 -0300

    feat: configs material ui theme, css basicline

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#232323",
+    },
+    secondary: {
+      main: "#4f8e3e",
+    },
+  },
+  typography:{
+
+    fontFamily:"segoe UI"
+  }
+});
+
+export default theme;
